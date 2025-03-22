bbfdf59ccc51e4301cdd362e0761a92aacf1da39
macchiato <macchiato9988@gmail.com>
2025-03-22 19:40:29 +0800

    fix

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,16 +1,20 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
+      <div className="p-2 flex gap-2 items-center justify-between">
+        <div>
+          <Link to="/" className="[&.active]:font-bold">
+            Home
+          </Link>{" "}
+          <Link to="/about" className="[&.active]:font-bold">
+            About
+          </Link>
+        </div>
+        <ModeToggle />
       </div>
       <hr />
       <Outlet />
